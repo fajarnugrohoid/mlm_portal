a2c87fffc2d0d0e90f3e05bf269d06950ae34919
israjHaliri <israj.haliri@gmail.com>
2016-01-22 16:29:07 +0700

    adding js not 100%

--- application/views/frontend_view/layout/footer.php
+++ application/views/frontend_view/layout/footer.php
@@ -1,6 +1,25 @@
 
 </body>
-<script type="text/javascript" src="<?php echo base_url('assets/js/jquery-2.2.0.min.js')?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/frontend/vendor/jquery-1.11.0.min.js')?>"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/bootstrap.js')?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/frontend/plugins/jquery.easing.1.3.js')?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/frontend/plugins/jquery.scrollto.js')?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/frontend/plugins/rs-plugin/js/jquery.themepunch.plugins.min.js')?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/frontend/plugins/rs-plugin/js/jquery.themepunch.revolution.min.js')?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/frontend/plugins/jquery.parallax-1.1.3.js')?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/frontend/plugins/jquery.countTo.js')?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/frontend/plugins/fancybox/jquery.mousewheel-3.0.6.pack.js')?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/frontend/plugins/fancybox/jquery.fancybox.js')?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/frontend/plugins/fancybox/helpers/jquery.fancybox-media.js')?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/frontend/plugins/jquery.isotope.js')?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/frontend/plugins/owl/owl.carousel.min.js')?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/frontend/plugins/jquery.knob.js')?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/frontend/plugins/gmaps.js')?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/frontend/plugins/waypoints.min.js')?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/frontend/plugins/uikit/uikit.js')?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/frontend/plugins/retina-1.1.0.js')?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/frontend/')?>"></script>
+<script type="text/javascript" src="<?php echo base_url('assets/js/frontend/')?>"></script>
+<script src="http://maps.google.com/maps/api/js?sensor=true"></script>	
 <script type="text/javascript" src="<?php echo base_url('assets/js/frontend/custom.js')?>"></script>
 </html>
